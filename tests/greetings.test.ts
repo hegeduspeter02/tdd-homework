1d03a42f8613d89c242468a0f5689c2d159faea4
Hegedűs Péter <hegipeti25@gmail.com>
2024-11-17 10:01:32 +0100

    test with an empty array

--- tests/greetings.test.ts
+++ tests/greetings.test.ts
@@ -1,5 +1,7 @@
 import { greetings } from '../src/greetings';
 
 describe('greetings', () => {
-    
+    test('with an empty array', () => {
+        expect(greetings([])).toBe('Hello, my friend');
+    });
 });

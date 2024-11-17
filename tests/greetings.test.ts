d53b7e1a73b49325b638e0df1b88199d2ffd7c1a
Hegedűs Péter <hegipeti25@gmail.com>
2024-11-17 12:34:55 +0100

    test case refactor, better grouping, added more cases

--- tests/greetings.test.ts
+++ tests/greetings.test.ts
@@ -2,11 +2,13 @@ import { greetings } from '../src/greetings';
 
 describe('greetings', () => {
 
-    describe('if no shouting happens', () => {
-        test('with an empty array', () => {
+    describe('with an empty array', () => {
+        it('should return the normal greeting', () => {
             expect(greetings([])).toBe('Hello, my friend');
         });
-    
+    });
+
+    describe('if no shouting happens', () => {
         test('with one name', () => {
             expect(greetings(['Alice'])).toBe('Hello Alice.');
         });
@@ -25,7 +27,21 @@ describe('greetings', () => {
             expect(greetings(['ALICE'])).toBe('HELLO ALICE!');
         });
 
-        test('with mixed inputs', () => {
+        test('with two names', () => {
+            expect(greetings(['ALICE', 'BOB'])).toBe('HELLO ALICE AND BOB!');
+        });
+
+        test('with more than two names', () => {
+            expect(greetings(['ALICE', 'BOB', 'CHARLIE', 'PETER'])).toBe('HELLO ALICE, BOB, CHARLIE AND PETER!');
+        });
+    });
+
+    describe('if normal and shouting greetings are mixed', () => {
+        test('with two mixed inputs', () => {
+            expect(greetings(['PETER', 'Charlie'])).toBe('Hello Charlie. HELLO PETER!');
+        });
+
+        test('with multiple mixed inputs', () => {
             expect(greetings(['Alice', 'BOB', 'Charlie', 'Peter', 'DANIEL'])).toBe('Hello Alice, Charlie and Peter. HELLO BOB AND DANIEL!');
         });
     });

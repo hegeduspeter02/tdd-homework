a099bea8fb403ad340328a2a4a2e8b8138d74163
Hegedűs Péter <hegipeti25@gmail.com>
2024-11-17 10:39:34 +0100

    test case written for shouting with one name

--- tests/greetings.test.ts
+++ tests/greetings.test.ts
@@ -1,19 +1,29 @@
 import { greetings } from '../src/greetings';
 
 describe('greetings', () => {
-    test('with an empty array', () => {
-        expect(greetings([])).toBe('Hello, my friend');
+
+    describe('if no shouting happens', () => {
+        test('with an empty array', () => {
+            expect(greetings([])).toBe('Hello, my friend');
+        });
+    
+        test('with one name', () => {
+            expect(greetings(['Alice'])).toBe('Hello Alice.');
+        });
+    
+        test('with two names', () => {
+            expect(greetings(['Alice', 'Bob'])).toBe('Hello Alice and Bob.');
+        });
+    
+        test('with more than two names', () => {
+            expect(greetings(['Alice', 'Bob', 'Charlie', 'Peter'])).toBe('Hello Alice, Bob, Charlie and Peter.');
+        });
     });
 
-    test('with one name', () => {
-        expect(greetings(['Alice'])).toBe('Hello Alice.');
+    describe('if shouting happens', () => {
+        test('with one name', () => {
+            expect(greetings(['ALICE'])).toBe('HELLO ALICE!');
+        });
     });
-
-    test('with two names', () => {
-        expect(greetings(['Alice', 'Bob'])).toBe('Hello Alice and Bob.');
-    });
-
-    test('with more than two names', () => {
-        expect(greetings(['Alice', 'Bob', 'Charlie', 'Peter'])).toBe('Hello Alice, Bob, Charlie and Peter.');
-    });
+    
 });
